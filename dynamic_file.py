a34e615e9cd2b7964bfbffdeacce5237d988a84d
rohitsharma93 <rs11010001@gmail.com>
2024-07-25 12:12:13 +0530

    dynamic_file.py

--- dynamic_file.py
+++ dynamic_file.py
@@ -1,4 +1,1 @@
-# dynamic_file.py
-import datetime
-
-dynamic_variable = f"This variable was last updated on {datetime.datetime.now()}"
+dynamic_variable = 'This variable was last updated on Thursday 25 July 2024 12:10:32 PM IST'
