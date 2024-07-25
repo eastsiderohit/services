16a634ab7dec15db595d790f46acc16f31c840e7
rohitsharma93 <rs11010001@gmail.com>
2024-07-25 11:50:07 +0530

    removed unnecessart file

--- zk-web/conf/zk-web-conf.clj
+++
@@ -1,5 +0,0 @@
-{
- :server-port 8081
- :users {"admin" "Admin@123"}
- :default-node ""
-}
--- zk-web/resources/public/js/functions.js
+++
@@ -1,5 +0,0 @@
-function copyToClipboard() {
-    var text = document.getElementById("clipboard-input");
-    text.select();
-    document.execCommand("copy");
-}
